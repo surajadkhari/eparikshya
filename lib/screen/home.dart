85970fe83d6439ecb6e677c54c0fc38e28b79f8c
surajadkhari <surajadkhari@gmail.com>
2021-10-25 12:36:23 +0545

    Add bottom nav bar

---
+++ lib/screen/home.dart
@@ -0,0 +1,46 @@
+import 'package:eparikshya/model/course_model.dart';
+import 'package:eparikshya/sections/course_card_section/course_card_section.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text(
+          "Eparikshya",
+          style: GoogleFonts.poppins(),
+        ),
+        leading: Icon(Icons.menu),
+      ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 345,
+            child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: Courses.course.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: CourseCard(
+                      index: index,
+                    ),
+                    margin: EdgeInsets.only(top: 8.0, left: 8.0),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -3,6 +3,9 @@ import 'package:eparikshya/sections/course_card_section/course_card_section.dart
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+import 'home.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -12,36 +15,54 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  int _selectedindex = 0;
+
+  static List<Widget> pages = <Widget>[home(), Container(), Container()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedindex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: AppBar(
-        title: Text(
-          "Eparikshya",
-          style: GoogleFonts.poppins(),
-        ),
-        leading: Icon(Icons.menu),
-      ),
-      body: ListView(
-        children: [
-          SizedBox(
-            height: 345,
-            child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: Courses.course.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    child: CourseCard(
-                      index: index,
-                    ),
-                    margin: EdgeInsets.only(top: 8.0, left: 8.0),
-                  );
-                }),
-          ),
-        ],
-      ),
-    );
+        backgroundColor: Colors.grey,
+        // appBar: AppBar(
+        //   title: Text(
+        //     "Eparikshya",
+        //     style: GoogleFonts.poppins(),
+        //   ),
+        //   leading: Icon(Icons.menu),
+        // ),
+        body: pages[_selectedindex],
+        // ListView(
+        //   children: [
+        //     SizedBox(
+        //       height: 345,
+        //       child: ListView.builder(
+        //           shrinkWrap: true,
+        //           scrollDirection: Axis.horizontal,
+        //           itemCount: Courses.course.length,
+        //           itemBuilder: (context, index) {
+        //             return Container(
+        //               child: CourseCard(
+        //                 index: index,
+        //               ),
+        //               margin: EdgeInsets.only(top: 8.0, left: 8.0),
+        //             );
+        //           }),
+        //     ),
+        //   ],
+        // ),
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedindex,
+            onTap: _onItemTapped,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(icon: Icon(Icons.book), label: "Courses"),
+              BottomNavigationBarItem(icon: Icon(Icons.save), label: "Saved")
+            ]));
   }
 }
